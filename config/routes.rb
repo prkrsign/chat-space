61217816214a38d157c6af16dba6cfef1a6521e0
prkrsign <prkrsign@yahoo.co.jp>
2019-07-18 01:48:30 +0900

    Add Grop Function
    Routing
    View
    Group create
    Side-bar View

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'messages#index'
-  resources :users , only:[:edit,:update,:show]
+  resources :users  , only:[:edit,:update,:show]
+  resources :groups , only:[:edit,:new,:create,:update] do
+    resources :messages
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
