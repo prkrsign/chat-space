c90ee0bac6dffd3c372a43a80a7a9e7f9a987392
prkrsign <prkrsign@yahoo.co.jp>
2019-07-19 06:09:12 +0900

    messages routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'groups#index'
+  root to:  "groups#index"
   resources :users  , only:[:edit,:update,:show]
   resources :groups , only:[:edit,:new,:create,:update] do
-    resources :messages
+    resources :messages , only: [:index , :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
