e1f1a68b79d3d44b93a55b2e97e7a13a62566234
prkrsign <prkrsign@yahoo.co.jp>
2019-07-20 19:09:48 +0900

    add Message Send Function2

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.action_view.field_error_proc = proc { |html_tag, instance| html_tag }
     config.i18n.default_locale = :ja
 
     config.generators do |g|
