0d883a0bb73fcc5d4e4ce2aca0c22e5bf0cf1b2b
prkrsign <prkrsign@yahoo.co.jp>
2019-07-27 13:41:48 +0900

    Preparation　Routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+    def index
+    end
+
     def update
         if current_user.update(user_params)
             redirect_to root_path , notice: "アカウント情報を編集しました"
@@ -9,6 +12,15 @@ class UsersController < ApplicationController
     end
 
     def edit
+    end
+
+    def search
+        @users = User.where('name LIKE(?)', "%#{params[:keyword]}%") #paramsとして送られてきたkeyword（入力された語句）で、Userモデルのnameカラムを検索し、その結果を@usersに代入する
+        respond_to do |format| 
+            format.html {redirect_to "users_path"}
+            format.json {render 'index', json: @users}
+            # format.json { render 'index', json: @users } #json形式のデータを受け取ったら、@usersをデータとして返す そしてindexをrenderで表示する
+        end
     end
 
     private
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  attr_accessor :keyword
   has_many :messages
   has_many :groups, through: :group_users
   has_many :group_users
