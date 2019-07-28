6bfc69a2b8668010bd1d4a32b59cdce894ebd00b
prkrsign <prkrsign@yahoo.co.jp>
2019-07-28 12:40:02 +0900

    Add chat-member-name and remove-name from search-result

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,7 @@
 $(document).on('turbolinks:load', function(){
 
     var search_list = $("#user-search-result");
+    var member_list = $("#member-append");
 
     function appendUser(user){
         var html = 
@@ -17,6 +18,16 @@ $(document).on('turbolinks:load', function(){
                         <p class="chat-group-user__name">${msg}</p>
                     </div>`;
                     search_list.append(html);
+    }
+
+    function addUser(user_id , user_name){
+        var html =
+                    `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                        <input name='group[user_ids][]' type='hidden' value=${user_id}>
+                        <p class='chat-group-user__name'>${user_name}</p>
+                        <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                    </div>`
+                    member_list.append(html);
     }
 
     $('#user-search-field').on('keyup', function(e){
@@ -55,10 +66,14 @@ $(document).on('turbolinks:load', function(){
         });
     });
 
-    $('.chat-group-form__field--right').on('click', function() {
-        console.log("押されたよ")
-    });
+    $(document).on("click", ".user-search-add", function () {  
+        // .on(events , [selector]) 第二引数では子要素を指定することによりイベントデリゲートできる
+          var user_id = $(this).data('user-id');
+          var user_name = $(this).data('user-name');
+          console.log(user_id);
+          console.log(user_name);
+          addUser(user_id , user_name);
+          $(this).parent().remove();
+    });    
 
-
-    
 });
