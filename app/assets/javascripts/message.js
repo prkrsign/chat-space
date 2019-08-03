619937650e9fc067625e31ca3c7bc5576f1071fa
prkrsign <prkrsign@yahoo.co.jp>
2019-08-03 12:18:39 +0900

    Create Auto-Reload-Function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(document).on('turbolinks:load', function(){
     function buildHTML(message) {
       var content = message.content ? `${ message.content }` : "";
-      var img  = message.image ? `<img src= "${ message.image }">` : "";
-      var html = `<div class="message" data-message_id="${message.id}">
+      var img  = message.image ? `<img class="lower-info__image" src="${ message.image }">` : "";
+      var html = `<div class="message" data-message-id="${message.id}">
                     <div class="upper-info">
                       <p class="upper-info__user">
                         ${message.user_name}
@@ -15,60 +15,63 @@ $(document).on('turbolinks:load', function(){
                         <p class="lower-info__content">
                             ${content}
                         </p>
-                        <p class ="lower-info__image">
                             ${img}
-                        </p>
                       </div>
                   </div>`
     return html;
     }
 
-    $('#new_message').on('submit', function(e){
-      e.preventDefault();
-      var message = new FormData(this);
-      var url = $(this).attr('action')
-      $.ajax({  
-        url: url,
-        type: 'POST',
-        data: message,
-        dataType: 'json',
-        processData: false,
-        contentType: false
+      $('#new_message').on('submit', function(e){
+        e.preventDefault();
+        var message = new FormData(this);
+        var url = $(this).attr('action')
+        $.ajax({  
+          url: url,
+          type: 'POST',
+          data: message,
+          dataType: 'json',
+          processData: false,
+          contentType: false
+        })
+
+        .done(function(data){
+          var html = buildHTML(data);
+          $('.messages').append(html);
+          $('.form__submit').attr('disabled', false);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+          $('#new_message')[0].reset();
+          return false
+        })
+        .fail(function(data){
+          alert('エラーが発生したためメッセージは送信できませんでした。');
+          $('.form__submit').attr('disabled', false);
+        })
       })
 
-      .done(function(data){
-        var html = buildHTML(data);
-        $('.messages').append(html);
-        $('.form__submit').attr('disabled', false);
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-        $('#new_message')[0].reset();
-        return false
-      })
-      .fail(function(data){
-        alert('エラーが発生したためメッセージは送信できませんでした。');
-        $('.form__submit').attr('disabled', false);
-      })
-    })
-
-    var reloadMessages = function(){
-      last_message_id = $('.messages:last').data('id');
+      // ここから自動更新機能
+      var reloadMessages = function(){
+      var href = 'api/messages#index {:format=>"json"}'
+      var last_message_id = $('.message:last').data('message-id');
+      
       $.ajax({
-        url:  location.href.json,
-        type: 'get',
-        dataType: 'json',
-        data: {id: last_message_id}
+        url:  href,
+        type: 'GET',
+        data: {id: last_message_id},
+        dataType: 'json'
       })
+      
+
       .done(function(messages){
         var insertHTML='';
-        messages.forEach(function(message){
-          insertHTML = buildHTML(message);
-          $('.message').append(insertHTML)
-          ScrollToNewMessage();
-        });
+          messages.forEach(function(message){
+            insertHTML = buildHTML(message);
+            $('.messages').append(insertHTML);
+            $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+          });
       })
       .fail(function(){
-        console.log('error')
+        alert("自動更新に失敗しました")
       });
-      setInterval(reloadMessages, 5000);
     };
-  });
+  setInterval(reloadMessages, 5000);
+});
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,9 +1,13 @@
 class Api::MessagesController < ApplicationController
     def index
-        @messages = Message.all
-        # ここから追記
-        respond_to do |format| 
-          format.html # html形式でアクセスがあった場合は特に何もなし(@messages = Message.allして終わり）
-          format.json { @new_message = Message.where('id > ?', params[:message][:id]) } # json形式    
+
+        @group = Group.find(params[:group_id])
+        @messages = @group.messages.includes(:user).where('id > ?', params[:id])
+        
+        respond_to do |format|
+            format.html
+            format.json
+        end
     end
 end
+#   format.json { @new_message = Message.where('id > ?', params[:message][:id]) } # json形式   
