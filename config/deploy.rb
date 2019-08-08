1ab02587c8781365251e78797f87a10e58fd382a
prkrsign <prkrsign@yahoo.co.jp>
2019-08-09 08:19:26 +0900

    Fix zdesc

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  zdesc 'upload secrets.yml'
+  desc 'upload secrets.yml'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
